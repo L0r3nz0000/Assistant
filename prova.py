11e0802203f07a8dcd20b731c2a9db9cfcc2335a
L0r3nz0000 <lorenzobefera0@gmail.com>
2024-08-21 23:59:06 +0200

    _

--- prova.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-subprocess.Popen(['play', 'media/timer.mp3'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-print("Sto facendo altre cose mentre l'audio viene riprodotto...")
