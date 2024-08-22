b6c2ab90c51c1c47a3e0b0b77ce7ec60f769f1d3
L0r3nz0000 <lorenzobefera0@gmail.com>
2024-08-23 00:42:28 +0200

    _

--- wake_word.py
+++ wake_word.py
@@ -2,7 +2,6 @@ from processes import kill_process_and_children
 import pvporcupine
 import pyaudio
 import struct
-import sys
 import os
 
 access_key = "KLlwjiQgPwLdfVFeikjfBtM/+8GnlLdCvlQaLAtUwUVDDr4jPNEgdw=="
@@ -15,7 +14,6 @@ def get_next_audio_frame(): pass
 # Aspetta la parola di attivazione
 def blocking_wake_word(conversation_open, response_completed):
   print('\x1b[34m\x1b[1m')
-
   handle = pvporcupine.create(
     access_key=access_key,
     keyword_paths=[keyword_path],
