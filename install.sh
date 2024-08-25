716a8ed68dbca00b76f82a02b12eba07e8687e53
Lorenzo <lorenzobefera0@gmail.com>
2024-08-25 15:24:52 +0200

    Creato servizio systemd e modifiche a start.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,27 @@ sudo su
 apt update
 apt install python3 python3-pip portaudio19-dev pulseaudio sox flac -y
 
-python3 -m venv .venv
+if [ -d ".venv" ]; then
+  python3 -m venv .venv
+fi
 source .venv/bin/activate
 pip install -r requirements.txt
 
+cd /etc/systemd/system/
+
+echo "[Unit]
+Description=Assistente vocale
+After=network.target
+
+[Service]
+ExecStart=$(pwd)/start.sh
+WorkingDirectory=$(pwd)
+User=$(whoami)
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+" > Assistant.service
+
+systemctl daemon-reload
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 pulseaudio --start
-source /home/lorenzo/Assistant/.venv/bin/activate
+source .venv/bin/activate
 python3 main.py
