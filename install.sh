6abed0e820b95da781791b9cf49d42a7b6f2d25d
Lorenzo <lorenzobefera0@gmail.com>
2024-08-25 16:40:16 +0200

    Modifiche a install.sh

--- install.sh
+++ install.sh
@@ -14,14 +14,15 @@ source .venv/bin/activate
 pip install -r requirements.txt
 
 read -s -p "Inserisci il token api di replicate: " API_TOKEN
+echo ""
 
-echo '[Unit]
+echo "[Unit]
 Description=Assistente vocale
 After=network.target
 
 [Service]
 ExecStart=$(pwd)/.venv/bin/python3 $(pwd)/main.py
-Environment="REPLICATE_API_TOKEN=$API_TOKEN"
+Environment=\"REPLICATE_API_TOKEN=$API_TOKEN\"
 WorkingDirectory=$(pwd)
 User=$(whoami)
 Restart=always
@@ -29,6 +30,6 @@ RestartSec=5
 
 [Install]
 WantedBy=multi-user.target
-' | sudo tee /etc/systemd/system/Assistant.service > /dev/null
+" | sudo tee /etc/systemd/system/Assistant.service > /dev/null
 
 sudo systemctl daemon-reload
